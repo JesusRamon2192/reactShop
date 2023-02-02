af5ee084eb2ababa5a1a4e40d5a8666b7486735c
jesusRamon <jesus.ramon2192@gmail.com>
2023-02-02 17:38:21 -0600

    Clase 23 Completando el carrito de compras

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AppContext from "@context/AppContext";
 import '@styles/Header.scss';
 import Menu from '@components/Menu';
 import logoMenu from '@icons/icon_menu.svg';
@@ -7,6 +8,7 @@ import logoCar from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const {state} = useContext(AppContext);
 
   const handleToggle = () => {
     setToggle(!toggle);
@@ -43,7 +45,7 @@ const Header = () => {
           <li className="navbar-email" onClick={handleToggle}>platzi@example.com</li>
           <li className="navbar-shopping-cart">
             <img src={logoCar} alt="shopping cart" />
-            <div>2</div>
+            {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
           </li>
         </ul>
       </div>
