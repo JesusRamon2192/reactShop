249913646593cc50f3d9e1c80075728fffd05b86
jesusRamon <jesus.ramon2192@gmail.com>
2023-02-03 19:02:51 -0600

    Deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'reactshop.git/bundle.js',
-        publicPath: '/reactshop/'
+        filename: 'bundle.js',
+        publicPath: './'
     },
     mode: 'development',
     resolve: {
