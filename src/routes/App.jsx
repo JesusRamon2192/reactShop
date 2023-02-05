75c6a51feb28dc34215663da470990a95fb1f9dc
jesusRamon <jesus.ramon2192@gmail.com>
2023-02-04 18:19:46 -0600

    Deploy

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
     const initialState = useInitialState();
     return (
         <AppContext.Provider value = {initialState}>
-        <HashRouter basename="/reactshop">
+        <HashRouter basename={process.env.reactshop}>
             <Layout>
                 <Routes>
                     <Route exact path="/" element={<Home/>} />
